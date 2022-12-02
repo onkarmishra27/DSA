525b59f27dae7ef03bf87194fb5aa929fa9666a9
Onkar Mishra <onkarmishra27@gmail.com>
2022-12-02 10:25:08 +0530

    for loopps started

---
+++ For Loops/Basic_for_loops.py
@@ -0,0 +1,21 @@
+# All Questions are from https://www.w3resource.com/c-programming-exercises/for-loop/index.php
+
+# 1. Write a program in C to display the first 10 natural numbers. Go to the editor
+# Expected Output :
+# 1 2 3 4 5 6 7 8 9 10
+
+# for i in range(1, 11):
+#     print(i)
+
+# 2. Write a C program to find the sum of first 10 natural numbers. Go to the editor
+# Expected Output :
+# The first 10 natural number is :
+# 1 2 3 4 5 6 7 8 9 10
+# The Sum is : 55
+
+
+# sum = 0
+# for i in range(1, 11):
+#     sum = sum + i
+
+# print("The Sum is: ", sum)
